c5a118ddf85bd9dc8ebf737e2048747b7b59b2bb
jjustinn53 <jjustinn53@gmail.com>
2025-03-10 15:18:24 -0700

    setup

--- app/src/main/java/com/jstn/csumb338/MainActivity.java
+++ app/src/main/java/com/jstn/csumb338/MainActivity.java
@@ -1,24 +1,29 @@
 package com.jstn.csumb338;
 
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.jstn.csumb338.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        View view = binding.getRoot();
+
+        setContentView(view);
+
+        binding.cToFTextView.setText("WOW IT WORKED");
+
+
     }
 }
